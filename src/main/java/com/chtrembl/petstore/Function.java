63a474679679cc0c299eb10b026fd686c86653f8
Denys Valkovyi <denys.valkovyi@cantire.com>
2024-03-31 11:48:18 +0200

    updated function app to work with service bus

--- src/main/java/com/chtrembl/petstore/Function.java
+++ src/main/java/com/chtrembl/petstore/Function.java
@@ -6,44 +6,31 @@ import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
 import com.microsoft.azure.functions.ExecutionContext;
-import com.microsoft.azure.functions.HttpMethod;
-import com.microsoft.azure.functions.HttpRequestMessage;
-import com.microsoft.azure.functions.HttpResponseMessage;
-import com.microsoft.azure.functions.HttpStatus;
-import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
-import com.microsoft.azure.functions.annotation.HttpTrigger;
-
-import java.util.Optional;
+import com.microsoft.azure.functions.annotation.ServiceBusTopicTrigger;
 
 /**
  * Azure Functions with HTTP Trigger.
  */
 public class Function {
-    @FunctionName("reserveorder")
-    public HttpResponseMessage run(
-            @HttpTrigger(
-                name = "req",
-                methods = {HttpMethod.POST},
-                authLevel = AuthorizationLevel.ANONYMOUS)
-                HttpRequestMessage<Optional<String>> request,
-            final ExecutionContext context) {
-        context.getLogger().info("Java HTTP trigger processed a request.");
 
-        final String bodyJson = request.getBody().orElse("Empty");
-        context.getLogger().info("Body: " + request.getBody());
+    @FunctionName("reserveorder")
+    public void run(
+            @ServiceBusTopicTrigger(
+                    name = "orders",
+                    topicName = "orders",
+                    subscriptionName = "functionapp",
+                    connection = "PETSTOREORDERSERVICE_EMAIL_TOPIC_CONNECTION_STRING"
+            ) String message,
+            final ExecutionContext context) {
+        context.getLogger().info("Java Service Bus trigger processed a message.");
+        context.getLogger().info("Message: " + message);
         
         String connectionString = System.getenv("AZURE_STORAGE_CONNECTION_STRING");
 
         BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connectionString).buildClient();
         BlobContainerClient blobContainerClient = blobServiceClient.getBlobContainerClient("reservationcontainer");
         BlobClient blobClient = blobContainerClient.getBlobClient("reservation.json");
-        blobClient.upload(BinaryData.fromString(bodyJson), true);
-
-        if (bodyJson.equals("Empty")) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass data in the request body").build();
-        } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Success").build();
-        }
+        blobClient.upload(BinaryData.fromString(message), true);
     }
 }
