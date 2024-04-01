6e3e2893dee3796ef2c850a1e8b3bfcae592dfec
Denys Valkovyi <denys.valkovyi@cantire.com>
2024-04-01 11:08:50 +0200

    updated function app for fallback scenario with logic app

--- src/main/java/com/chtrembl/petstore/Function.java
+++ src/main/java/com/chtrembl/petstore/Function.java
@@ -1,18 +1,34 @@
 package com.chtrembl.petstore;
 
+import com.azure.core.http.HttpResponse;
 import com.azure.core.util.BinaryData;
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.models.BlobErrorCode;
+import com.azure.storage.blob.models.BlobStorageException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.ServiceBusTopicTrigger;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.Optional;
+import java.util.logging.Level;
 
 /**
  * Azure Functions with HTTP Trigger.
  */
 public class Function {
+
+    public static final int RETRY_COUNT = 3;
 
     @FunctionName("reserveorder")
     public void run(
@@ -23,14 +39,66 @@ public class Function {
                     connection = "SERVICE_BUS_ORDERS_TOPIC_CONNECTION_STRING"
             ) String message,
             final ExecutionContext context) {
-        context.getLogger().info("Java Service Bus trigger processed a message.");
-        context.getLogger().info("Message: " + message);
-        
-        String connectionString = System.getenv("AZURE_STORAGE_CONNECTION_STRING");
+        context.getLogger().info("Java Service Bus trigger processed a message: " + message);
 
-        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connectionString).buildClient();
-        BlobContainerClient blobContainerClient = blobServiceClient.getBlobContainerClient("reservationcontainer");
-        BlobClient blobClient = blobContainerClient.getBlobClient("reservation.json");
-        blobClient.upload(BinaryData.fromString(message), true);
+        String orderId = getOrderIdFromMessage(message, context);
+
+        boolean uploaded = false;
+        for (int i = 0; i < RETRY_COUNT; i++) {
+            uploaded = tryUploadToBlob(message, context, orderId);
+            if (uploaded) {
+                break;
+            }
+        }
+        if (!uploaded) {
+            HttpStatusCode statusCode = uploadToLogicApps(message);
+            if (statusCode.is2xxSuccessful()) {
+                context.getLogger().info("Successfully triggered fallback scenario. Sending order details to email.");
+            } else {
+                context.getLogger().log(Level.SEVERE, "Fallback scenario fails: " + statusCode);
+            }
+        }
+    }
+
+    private HttpStatusCode uploadToLogicApps(String body) {
+        String logicAppUrl = System.getenv("LOGIC_APP_URL");
+        WebClient.ResponseSpec response = WebClient.builder().build().post().uri(logicAppUrl)
+                .body(BodyInserters.fromPublisher(Mono.just(body), String.class))
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                .header("Cache-Control", "no-cache")
+                .retrieve();
+        return Optional.of(response.toBodilessEntity().block().getStatusCode()).get();
+    }
+
+    private static boolean tryUploadToBlob(String message, ExecutionContext context, String orderId) {
+        String connectionString = System.getenv("AZURE_STORAGE_CONNECTION_STRING");
+        try {
+            BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connectionString).buildClient();
+            BlobContainerClient blobContainerClient = blobServiceClient.getBlobContainerClient("reservationcontainer");
+            BlobClient blobClient = blobContainerClient.getBlobClient(orderId + ".json");
+            blobClient.upload(BinaryData.fromString(message), true);
+            return true;
+        } catch (BlobStorageException e) {
+            HttpResponse response = e.getResponse();
+            context.getLogger().log(Level.SEVERE, "Uploading failed with Http status code: " + response.getStatusCode());
+            if (e.getErrorCode() == BlobErrorCode.RESOURCE_NOT_FOUND) {
+                context.getLogger().log(Level.SEVERE, "Extended details: " + e.getStatusCode());
+            } else if (e.getErrorCode() == BlobErrorCode.CONTAINER_BEING_DELETED) {
+                context.getLogger().log(Level.SEVERE, "Extended details: " + e.getServiceMessage());
+            }
+            return false;
+        }
+    }
+
+    private static String getOrderIdFromMessage(String message, ExecutionContext context) {
+        JsonNode jsonNode = null;
+        try {
+            jsonNode = new ObjectMapper().readTree(message);
+        } catch (JsonProcessingException e) {
+            context.getLogger().log(Level.SEVERE, "Cannot parse message from service bus: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+        return jsonNode.get("id").asText();
     }
 }
